29e60e4e68f1ed4e87efcee71a62119d625b4355
HuangXW <huangxw@asiainfo.com>
2020-06-15 22:24:11 +0800

    zk、redis分布式锁

--- src/main/java/com/huangxw/redis/RedisLock.java
+++ src/main/java/com/huangxw/redis/RedisLock.java
@@ -1,5 +1,108 @@
 package com.huangxw.redis;
 
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.Collections;
+
+/**
+ * 单个服务去访问Redis的时候，redis本身单线程，不用考虑现场安全
+ * 但多个服务访问redis时，如秒杀场景，几个服务同时访问redis，会产生超卖问题
+ *
+ * redis分布式锁实现
+ */
 public class RedisLock {
+    private static final int NUM = 1000;
+
+    private String LOCK_KEY = "redis_lock"; //锁键
+
+    protected long INTERNAL_LOCK_LEAST_TIME = 2000; //锁过期时间  2s
+
+    private long timeout = 200000;  //获取锁的超时时间, 20s
+
+    private SetParams params = SetParams.setParams().nx().px(INTERNAL_LOCK_LEAST_TIME);
+
+    JedisPool jedisPool = new JedisPool("127.0.0.1",6379);
+
+
+    /**
+     *加锁
+     */
+    public boolean lock(String id){
+        Long start = System.currentTimeMillis();
+        Jedis jedis = jedisPool.getResource();
+        try
+        {
+            for (; ;)
+            {
+                //SET命令返回ok，则证明获取锁成功
+                String lock = jedis.set(LOCK_KEY,id,params);
+                if("OK".equals(lock))
+                {
+                    System.out.println("UUID: " + id + "加锁成功！！！！！！！！！");
+                    return true;
+                }
+
+                try
+                {
+                    Thread.sleep(20);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                //System.out.println("UUID: " + id + "加锁失败，等待加锁！！！！！！！！！");
+                //否则循环等待，在timeout时间内仍未获取到锁，则获取失败
+                long l = System.currentTimeMillis() - start;
+                if(l > timeout)
+                {
+                    System.out.println("UUID: " + id + "等待加锁超时！！！！！！！！！");
+                    return false;
+                }
+                else
+                {
+                    //System.out.println("UUID: " + id + "等待时间：" + l);
+                }
+            }
+        }
+        finally
+        {
+            jedis.close();
+        }
+    }
+
+    //解锁
+    public boolean unlock(String id){
+        Jedis jedis = jedisPool.getResource();
+        //lua脚本保证原子操作
+        String script =
+                "if redis.call('get',KEYS[1]) == ARGV[1] then " +
+                    "return redis.call('del',KEYS[1])" +
+                "else \n" +
+                    "return 0 \n" +
+                "end";
+
+        try
+        {
+            //Collections.singletonList(LOCK_KEY)用于只有一个元素的list优化，减少内存分配
+            Object result = jedis.eval(script,Collections.singletonList(LOCK_KEY),Collections.singletonList(id));
+            if("1".equals(result.toString()))
+            {
+                System.out.println("UUID: " + id + "解锁成功");
+                return true;
+            }
+            else
+            {
+                //System.out.println("UUID: " + id + "解锁失败！！！！！！！！！");
+                return false;
+            }
+        }
+        finally
+        {
+            jedis.close();
+        }
+    }
 
 }
--- src/main/java/com/huangxw/redis/RedisTest.java
+++ src/main/java/com/huangxw/redis/RedisTest.java
@@ -1,50 +1,108 @@
 package com.huangxw.redis;
 
+import org.junit.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
+
 
 /**
  * 实现redis分布式锁
  * 1.不用redis锁，单机时可通过reentrantLock.lock()、reentrantLock.unlock()实现
+ *
+ * 2.自己写RedisLock类，实现redis分布式锁
  */
 public class RedisTest {
 
     private static Integer inventory = 1001;
     private static final int NUM = 1000;
     private static LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue();
-    static ReentrantLock reentrantLock = new ReentrantLock();
+    static RedisLock redisLock = new RedisLock();
 
-    public static void main(String[] args) {
+    @Test
+    public void test() throws InterruptedException {
         ThreadPoolExecutor threadPoolExecutor =
                 new ThreadPoolExecutor(inventory,inventory,10L, TimeUnit.SECONDS,linkedBlockingQueue);
-        final CountDownLatch countDownLatch = new CountDownLatch(NUM);
         long start = System.currentTimeMillis();
+        CountDownLatch countDownLatch = new CountDownLatch(1000);
 
         for (int i = 0; i < NUM; i++)
         {
-            threadPoolExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    reentrantLock.lock();
+            threadPoolExecutor.execute(() -> {
+                String uuid = UUID.randomUUID().toString();  //使用UUID，保证谁加的锁，谁删除
+                try
+                {
+                    if(!redisLock.lock(uuid))
+                    {
+                        System.out.println("locked error！！！！！！");
+                        return;
+                    }
                     inventory--;
-                    reentrantLock.unlock();
-                    System.out.println("线程执行：" + Thread.currentThread().getName());
-                    //将count值减1
-                    countDownLatch.countDown();
+                    System.out.println(inventory);
+                    //System.out.println("线程执行：" + Thread.currentThread().getName());
+                }finally
+                {
+                    redisLock.unlock(uuid);
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("线程执行数：" + NUM + " 总耗时：" + (end - start) + " 库存数为:" + inventory);
+    }
+
+    @Test
+    public void testRedission() throws InterruptedException {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        RedissonClient redissonClient = Redisson.create(config);
+
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(inventory,inventory,10L, TimeUnit.SECONDS,linkedBlockingQueue);
+        long start = System.currentTimeMillis();
+        CountDownLatch countDownLatch = new CountDownLatch(1000);
+
+        for (int i = 0; i < NUM; i++)
+        {
+            threadPoolExecutor.execute(() -> {
+                String uuid = UUID.randomUUID().toString();  //使用UUID，保证谁加的锁，谁删除
+                RLock lock = redissonClient.getLock("lockName");
+                boolean res = false;
+                try
+                {   // 尝试加锁，最多等待2秒，上锁以后3秒自动解锁
+                    res = lock.tryLock(2,3, TimeUnit.SECONDS);
+                    if(res)
+                    {
+                        inventory--;
+                        System.out.println(inventory);
+                        countDownLatch.countDown();
+                    }
+                }
+                catch (InterruptedException e) {
+                    System.out.println("加锁失败！！！！！！");
+                }
+                finally
+                {
+                    lock.unlock();
                 }
             });
         }
-        threadPoolExecutor.shutdown();
-        try {
-            //调用await()方法的线程会被挂起，它会等待直到count值为0才继续执行
-            countDownLatch.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+
+        countDownLatch.await();
         long end = System.currentTimeMillis();
         System.out.println("线程执行数：" + NUM + " 总耗时：" + (end - start) + " 库存数为:" + inventory);
+
+
     }
 }
